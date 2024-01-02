b2edc805c5a09cdae9397048e3df94bd4c69add1
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-01-02 10:01:16 +0000

    DTSPO-15909 - migrated Application Insights

--- main.tf
+++ main.tf
@@ -21,32 +21,31 @@ module "key-vault" {
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_platformengineering group object ID
-  product_group_name         = "DTS RPTS"
-  common_tags                = var.common_tags
-  create_managed_identity    = true
+  product_group_name      = "DTS RPTS"
+  common_tags             = var.common_tags
+  create_managed_identity = true
 }
 
 resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
   name         = "AppInsightsInstrumentationKey"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
+  value        = module.application_insights.instrumentation_key
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-appinsights-${var.env}"
-  location            = var.appinsights_location
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-appinsights-"
+
   resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
 
-  tags = var.common_tags
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
+  common_tags = var.common_tags
+}
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
 }
 
 resource "azurerm_storage_account" "storage_account" {
@@ -63,20 +62,20 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_key_vault_secret" "storage_account_name" {
-  name      = "storage-account-name"
-  value     = azurerm_storage_account.storage_account.name
+  name         = "storage-account-name"
+  value        = azurerm_storage_account.storage_account.name
   key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "storage_account_primary_key" {
-  name      = "storage-account-primary-key"
-  value     = azurerm_storage_account.storage_account.primary_access_key
+  name         = "storage-account-primary-key"
+  value        = azurerm_storage_account.storage_account.primary_access_key
   key_vault_id = module.key-vault.key_vault_id
 }
 
 resource "azurerm_key_vault_secret" "storage_account_connection_string" {
-  name      = "storage-account-connection-string"
-  value     = azurerm_storage_account.storage_account.primary_connection_string
+  name         = "storage-account-connection-string"
+  value        = azurerm_storage_account.storage_account.primary_connection_string
   key_vault_id = module.key-vault.key_vault_id
 }
 
