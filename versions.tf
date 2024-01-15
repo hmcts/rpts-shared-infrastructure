53121e44051c61465c420a84aef2c72b089c9b1d
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-01-15 11:09:41 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,14 +1,3 @@
 terraform {
-  backend "azurerm" {}
-
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.85.0"
-    }
-  }
+  required_version = ">= 1.6.6"
 }
