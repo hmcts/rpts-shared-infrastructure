8e6c72d8e851fb96672d686e85e7557fc34858cc
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-01-15 11:22:25 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,6 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
-  allow_blob_public_access = true
 
   tags = var.common_tags
 }
