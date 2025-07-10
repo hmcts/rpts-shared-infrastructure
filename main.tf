98ddab070e1fe7850e31e70bca77be7aa6b939ab
Imran <Imran.Ali@justice.gov.uk>
2025-07-10 15:25:58 +0100

    FACT-2399 infrastructure changes

--- main.tf
+++ main.tf
@@ -13,6 +13,8 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "key-vault" {
+  count = contains(["aat", "demo", "prod"], var.env) ? 1 : 0
+
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product             = var.product
   env                 = var.env
@@ -27,12 +29,15 @@ module "key-vault" {
 }
 
 resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
+  count        = contains(["aat", "demo", "prod"], var.env) ? 1 : 0
   name         = "AppInsightsInstrumentationKey"
-  value        = module.application_insights.instrumentation_key
+  value        = module.application_insights[0].instrumentation_key
   key_vault_id = module.key-vault.key_vault_id
 }
 
 module "application_insights" {
+  count = contains(["aat", "demo", "prod"], var.env) ? 1 : 0
+
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env      = var.env
@@ -46,11 +51,12 @@ module "application_insights" {
 }
 moved {
   from = azurerm_application_insights.appinsights
-  to   = module.application_insights.azurerm_application_insights.this
+  to   = module.application_insights[0].azurerm_application_insights.this
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  count        = contains(["aat", "demo", "prod"], var.env) ? 1 : 0
   name         = "app-insights-connection-string"
-  value        = module.application_insights.connection_string
+  value        = module.application_insights[0].connection_string
   key_vault_id = module.key-vault.key_vault_id
 }
